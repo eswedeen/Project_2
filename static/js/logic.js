5fb8990b79abb1caf800b40bb1594adf48126ad0
tonyveliz <43831983+tonyveliz@users.noreply.github.com>
2019-04-03 18:34:12 -0500

    Updated Index HTML

--- static/js/logic.js
+++ static/js/logic.js
@@ -8,6 +8,8 @@ var myMap = L.map("map", {
     L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
       attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery � <a href=\"https://www.mapbox.com/\">Mapbox</a>",
       maxZoom: 18,
-      id: "mapbox.light",
+      id: "mapbox.dark",
       accessToken: API_KEY
     }).addTo(myMap);
+
+    
