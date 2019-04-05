60c6d24565a6de4a6534465f7515c527653d84c4
erik <erikswedeen@gmail.com>
2019-04-04 21:46:25 -0500

    updated app.js, index.html, added choropleth.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,29 +1,18 @@
 // Default Map Settings
-var defaultCenter = [39.74739, -105];
-var defaultZoom = 4;
+var defaultCenter = [45, 0];
+var defaultZoom = 2;
 //var bounds = L.latLngBounds([28.70, -127.50],[48.85, -55.90])
-
-// Endpoints for Country and Region JSON Data
-
-// geoJSON data & styling
-var myGeoJSONPath = '/geojson';
-var myCustomStyle = {
-    stroke: true,
-    fill: true,
-    fillColor: '#fff',
-    fillOpacity: 0.25    
-}
 
 // FUNCTION: To initialize dashboard
 function init() {
     //Get Geo JSON Data
-    d3.json(myGeoJSONPath, function(data) {
+    d3.json("/geojson", function(data) {
         //Load Geo JSON Data and Build Map
-        buildMap(data);
-        buildPie();
-        buildBar();
+    buildMap(data);
+    buildPie();
+    buildBar();
         
-    }); 
+    });
 }
 
 // FUNCTION: buildMap
@@ -36,16 +25,6 @@ function buildMap(data) {
         accessToken: API_KEY
     });
 
-    // Create baseMaps object to hold the lightmap layer
-    baseMaps = {
-        "World Map": worldMap
-    };
-
-    // var overlayMaps = {
-    //     Countries: countryData
-    //     Regions: regionData
-    // };
-
     // Create the map object with options
     var map = L.map("map-id", {
         center: defaultCenter,
@@ -53,16 +32,47 @@ function buildMap(data) {
         layers: [worldMap]
     });
 
-    // Create Layer Control
-    L.control.layers(baseMaps).addTo(map);
-
-    // Add GeoJSON Data
-    L.geoJson(data, {
+    // format and load the GeoJSON data
+    var myCustomStyle = {
+        stroke: true,
+        fill: true,
+        fillColor: '#0000ff',
+        fillOpacity: 0.75    
+    }
+    var countryMap = L.geoJson(data, {
         clickable: false,
         style: myCustomStyle
     }).addTo(map);
-    
+
+    // Create baseMaps object to hold the lightmap layer
+    baseMaps = {
+        "World Map": worldMap
+        
+    };
+    // Create overlayMaps object to hold the Country Data
+    var overlayMaps = {
+        "Production by Country": countryMap
+    };
+    // Create Layer Control
+    L.control.layers(baseMaps, overlayMaps).addTo(map);
 };
+
+// FUNCTION: geoData
+function geoData() {
+    d3.json("/geojson", function(data) {
+        // format and load the GeoJSON data
+        var myCustomStyle = {
+            stroke: true,
+            fill: true,
+            fillColor: '#0000ff',
+            fillOpacity: 0.75    
+        }
+        var countryMap = L.geoJson(data, {
+            clickable: false,
+            style: myCustomStyle
+        }).addTo(map);
+    });
+}
 
 // FUNCTION: To build charts
 function buildPie() {
