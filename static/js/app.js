61503ae5f92cb6bd6d758ab92a96c066cadc4298
al hambra <40449556+riro1974@users.noreply.github.com>
2019-04-04 20:26:28 -0500

    update all files from local

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,9 @@
-
 // Default Map Settings
-
 var defaultCenter = [39.74739, -105];
-
 var defaultZoom = 4;
 
 //var bounds = L.latLngBounds([28.70, -127.50],[48.85, -55.90])
+
 
 
 
@@ -13,23 +11,17 @@ var defaultZoom = 4;
 
 
 
+
 // geoJSON data & styling
 
-var myGeoJSONPath = '/geojson';
+var myGeoJSONPath = '/countries';
 
 var myCustomStyle = {
-
     stroke: true,
-
     fill: true,
-
     fillColor: '#fff',
-
     fillOpacity: 0.25    
-
 }
-
-
 
 // FUNCTION: To initialize dashboard
 
@@ -40,134 +32,102 @@ function init() {
     d3.json(myGeoJSONPath, function(data) {
 
         //Load Geo JSON Data and Build Map
+        var Year = data.year;
+        var 
+
 
         buildMap(data);
-
         buildCharts();
 
     }); 
-
 }
-
-
 
 // FUNCTION: buildMap
 
 function buildMap(data) {
-
     // Create the tile layer that will be the background of our map
-
     var worldMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-
         maxZoom: 18,
-
         id: "mapbox.dark",
-
         accessToken: API_KEY
-
     });
-
-
 
     // Create baseMaps object to hold the lightmap layer
 
     baseMaps = {
-
         "World Map": worldMap
-
     };
-
-
-
+    lightmap.addTo(map);
     // var overlayMaps = {
-
     //     Countries: countryData
-
     //     Regions: regionData
-
     // };
-
-
 
     // Create the map object with options
 
     var map = L.map("map-id", {
-
         center: defaultCenter,
-
         zoom: defaultZoom,
-
         layers: [worldMap]
-
     });
-
-
 
     // Create Layer Control
 
     L.control.layers(baseMaps).addTo(map);
 
-
-
     // Add GeoJSON Data
-
     L.geoJson(data, {
-
         clickable: false,
-
         style: myCustomStyle
-
     }).addTo(map);
 
+
+
     
+
+
 
 };
 
 
 
+
+
+
+
 // FUNCTION: To build charts
+
+
 
 function buildCharts() {
 
+
+
     // get year value from dropdown using d3
+
+
 
     var year = 1990;
 
     d3.json(`/countries/${year}`, function(data) {
 
-        console.log(year);
-
-        console.log(data);
-
-        console.log(data.yearKey);
-
-        console.log(data.yearKey[0]);
-
-        console.log(data.yearKey[0][0]);
-
-
-
-        var TEPList = data.yearKey[0];
-
-        console.log(TEPList);
-
-    
-
-        
-
-
+        console.log(date[0]);
 
     });
+
+
 
 }
 
 
 
+
+
+
+
 // Initialize Dashboard
 
+
+
 init();
-
-
-
